dea2494ddcfd03410471fd91bffa5731d847c035
Benjamin Cron <benjamin.cron.17@gmail.com>
2023-02-16 16:02:08 +0100

    Ajout des classes Planete et Vol

--- src/main/java/com/galacticat/galacticat/bo/Vol.java
+++ src/main/java/com/galacticat/galacticat/bo/Vol.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+
 @Data
 @Entity
 @Table(name="Vol")
@@ -12,16 +15,24 @@ public class Vol {
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "PLANETE_ID")
+    private Planete depart;
+    @OneToOne
+    @JoinColumn(name = "PLANETE_ID")
+    private Planete arrivée;
+    private LocalDateTime dateDepart;
     @OneToOne
     @JoinColumn(name = "VAISSEAU_ID")
     private Vaisseau vaisseau;
 
     @OneToOne
-    @JoinColumn(name = "PLANETE_ID")
-    private Planete depart;
+    @JoinColumn(name = "CASTRONAUTE_ID")
+    private Catstronaute pilote;
 
-    @OneToOne
-    @JoinColumn(name = "PLANETE_ID")
-    private Planete arrivée;
+    @OneToMany
+    @JoinColumn(name = "CASTRONAUTE_ID")
+    private Catstronaute passager;
 
 }
